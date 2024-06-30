450d98fdb687d9c11b7eab6288cea5dbccf0d8b6
emiliopalmerini <emilio.palmerini@proton.me>
2024-06-30 21:02:00 +0200

    feat(neovim): dap python

--- .config/nvim/lua/emilio/lazy/dap.lua
+++ .config/nvim/lua/emilio/lazy/dap.lua
@@ -4,6 +4,7 @@ return {
         "rcarriga/nvim-dap-ui",
         "theHamsta/nvim-dap-virtual-text",
         "nvim-telescope/telescope-dap.nvim",
+        "mfussenegger/nvim-dap-python",
     },
     config = function ()
         --require('dap').setup({})
