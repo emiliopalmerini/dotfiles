79d0aadfbaf0ef3c3bfc21f2e787091cb47d6c91
emiliopalmerini <emilio.palmerini@proton.me>
2024-07-20 11:53:34 +0200

    update

--- .config/nvim/lua/emilio/lazy/obsidian.lua
+++ .config/nvim/lua/emilio/lazy/obsidian.lua
@@ -116,7 +116,7 @@ return {
             local suffix = ""
             if title ~= nil then
                 -- If title is given, transform it into valid file name.
-                suffix = title:gsub(" ", "_"):gsub("[^A-Za-z0-9-]", ""):lower()
+                suffix = title:gsub(" ", "_"):gsub("-", "_"):gsub("[^A-Za-z0-9-]", ""):lower()
             else
                 -- If title is nil, just add 4 random uppercase letters to the suffix.
                 for _ = 1, 4 do
