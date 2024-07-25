0dfd298730c0d77b4fc79b96ede585ecd6616164
emiliopalmerini <emilio.palmerini@proton.me>
2024-07-25 15:10:13 +0200

    feat(zed): remove .* config files

--- .config/nvim/lua/emilio/lazy/dap.lua
+++ .config/nvim/lua/emilio/lazy/dap.lua
@@ -13,8 +13,8 @@ return {
         vim.keymap.set('n', '<F2>', ":lua require'dap'.step_over()<CR>", {buffer=0})
         vim.keymap.set('n', '<F3>', ":lua require'dap'.step_into()<CR>", {buffer=0})
         vim.keymap.set('n', '<F3>', ":lua require'dap'.step_out()<CR>", {buffer=0})
-        vim.keymap.set('n', '<leader>b', ":lua require'dap'.toggle_breakpoint()<CR>", {buffer=0})
-        vim.keymap.set('n', '<leader>B', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", {buffer=0})
+        vim.keymap.set('n', '<leader>bp', ":lua require'dap'.toggle_breakpoint()<CR>", {buffer=0})
+        vim.keymap.set('n', '<leader>Bp', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", {buffer=0})
         vim.keymap.set('n', '<leader>lp', ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", {buffer=0})
         vim.keymap.set('n', '<leader>dr', ":lua require'dap'.repl.open()<CR>", {buffer=0})
 
