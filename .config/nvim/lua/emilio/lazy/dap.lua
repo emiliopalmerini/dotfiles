bfe0fd61b219b91aa36801e3603d5033c661bf40
emiliopalmerini <emilio.palmerini@proton.me>
2024-08-27 20:52:02 +0200

    feat(nvim): add dap for go, elixir and dotnet

--- .config/nvim/lua/emilio/lazy/dap.lua
+++ .config/nvim/lua/emilio/lazy/dap.lua
@@ -1,56 +1,111 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "theHamsta/nvim-dap-virtual-text",
-        "nvim-telescope/telescope-dap.nvim",
-        "mfussenegger/nvim-dap-python",
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "leoluz/nvim-dap-go",
+            "rcarriga/nvim-dap-ui",
+            "theHamsta/nvim-dap-virtual-text",
+            "nvim-neotest/nvim-nio",
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            local dap = require "dap"
+            local ui = require "dapui"
+
+            require("dapui").setup()
+            require("dap-go").setup()
+
+            require("nvim-dap-virtual-text").setup {
+                -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+                display_callback = function(variable)
+                    local name = string.lower(variable.name)
+                    local value = string.lower(variable.value)
+                    if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+                        return "*****"
+                    end
+
+                    if #variable.value > 15 then
+                        return " " .. string.sub(variable.value, 1, 15) .. "... "
+                    end
+
+                    return " " .. variable.value
+                end,
+            }
+
+            -- Handled by nvim-dap-go
+            -- dap.adapters.go = {
+            --   type = "server",
+            --   port = "${port}",
+            --   executable = {
+            --     command = "dlv",
+            --     args = { "dap", "-l", "127.0.0.1:${port}" },
+            --   },
+            -- }
+
+            local elixir_ls_debugger = vim.fn.exepath "elixir-ls-debugger"
+            if elixir_ls_debugger ~= "" then
+                dap.adapters.mix_task = {
+                    type = "executable",
+                    command = elixir_ls_debugger,
+                }
+
+                dap.configurations.elixir = {
+                    {
+                        type = "mix_task",
+                        name = "phoenix server",
+                        task = "phx.server",
+                        request = "launch",
+                        projectDir = "${workspaceFolder}",
+                        exitAfterTaskReturns = false,
+                        debugAutoInterpretAllModules = false,
+                    },
+                }
+            end
+
+            dap.adapters.coreclr = {
+                type = 'executable',
+                command = '/usr/local/bin/netcoredbg/netcoredbg',
+                args = { '--interpreter=vscode' }
+            }
+
+            dap.configurations.cs = {
+                {
+                    type = "coreclr",
+                    name = "launch - netcoredbg",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+                    end,
+                },
+            }
+
+            vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+            vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+            -- Eval var under cursor
+            vim.keymap.set("n", "<space>?", function()
+                require("dapui").eval(nil, { enter = true })
+            end)
+
+            vim.keymap.set("n", "<F1>", dap.continue)
+            vim.keymap.set("n", "<F2>", dap.step_into)
+            vim.keymap.set("n", "<F3>", dap.step_over)
+            vim.keymap.set("n", "<F4>", dap.step_out)
+            vim.keymap.set("n", "<F5>", dap.step_back)
+            vim.keymap.set("n", "<F13>", dap.restart)
+
+            dap.listeners.before.attach.dapui_config = function()
+                ui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                ui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                ui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                ui.close()
+            end
+        end,
     },
-    config = function ()
-        --require('dap').setup({})
-
-        vim.keymap.set('n', '<F1>', ":lua require'dap'.continue()<CR>", {buffer=0})
-        vim.keymap.set('n', '<F2>', ":lua require'dap'.step_over()<CR>", {buffer=0})
-        vim.keymap.set('n', '<F3>', ":lua require'dap'.step_into()<CR>", {buffer=0})
-        vim.keymap.set('n', '<F3>', ":lua require'dap'.step_out()<CR>", {buffer=0})
-        vim.keymap.set('n', '<leader>bp', ":lua require'dap'.toggle_breakpoint()<CR>", {buffer=0})
-        vim.keymap.set('n', '<leader>Bp', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", {buffer=0})
-        vim.keymap.set('n', '<leader>lp', ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", {buffer=0})
-        vim.keymap.set('n', '<leader>dr', ":lua require'dap'.repl.open()<CR>", {buffer=0})
-
-        local dap = require('dap')
-        dap.adapters.delve = {
-            type = 'server',
-            port = '${port}',
-            executable = {
-                command = 'dlv',
-                args = {'dap', '-l', '127.0.0.1:${port}'},
-            }
-        }
-
-        -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
-        dap.configurations.go = {
-            {
-                type = "delve",
-                name = "Debug",
-                request = "launch",
-                program = "${file}"
-            },
-            {
-                type = "delve",
-                name = "Debug test", -- configuration for debugging test files
-                request = "launch",
-                mode = "test",
-                program = "${file}"
-            },
-            -- works with go.mod packages and sub packages
-            {
-                type = "delve",
-                name = "Debug test (go.mod)",
-                request = "launch",
-                mode = "test",
-                program = "./${relativeFileDirname}"
-            }
-        }
-    end
 }
