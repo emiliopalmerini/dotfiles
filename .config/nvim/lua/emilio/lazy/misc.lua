0da409a50533b3ebbabe1f604b589fc6e7de0243
emiliopalmerini <emilio.palmerini@proton.me>
2024-08-30 05:08:32 +0200

    feat(nvim): comment

--- .config/nvim/lua/emilio/lazy/misc.lua
+++ .config/nvim/lua/emilio/lazy/misc.lua
@@ -4,7 +4,11 @@ return {
         event = 'VimEnter',
         dependencies = { 'nvim-lua/plenary.nvim' },
         opts = { signs = false }
-    },{
+    },
+    {
         'tpope/vim-sleuth',
+    },
+    {
+        'numToStr/Comment.nvim',
     }
 }
