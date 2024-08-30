d13ce2712832148fd957a855eb446f0e4afb7d56
emiliopalmerini <emilio.palmerini@proton.me>
2024-08-30 05:17:39 +0200

    fix(nvim): improve snippets

--- .config/nvim/lua/emilio/lazy/snippets.lua
+++ .config/nvim/lua/emilio/lazy/snippets.lua
@@ -1,27 +1,34 @@
 return {
-    "L3MON4D3/LuaSnip",
-    version = "v2.*",
-    build = "make install_jsregexp",
-    dependencies = { "rafamadriz/friendly-snippets" },
-    config = function()
-        local ls = require("luasnip")
+    {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
 
-        vim.keymap.set({ "i" }, "<C-k>", function() ls.expand() end, { silent = true })
-        vim.keymap.set({ "i", "s" }, "<C-l>", function() ls.jump(1) end, { silent = true })
-        vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(-1) end, { silent = true })
+        dependencies = { "rafamadriz/friendly-snippets" },
 
-        vim.keymap.set({ "i", "s" }, "<C-e>", function()
-            if ls.choice_active() then
-                ls.change_choice(1)
-            end
-        end, { silent = true })
+        config = function()
+            local ls = require("luasnip")
+            ls.filetype_extend("javascript", { "jsdoc" })
 
-        --require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
-        require("luasnip.loaders.from_snipmate").load({ path = { "./my-snippets" } })
+            vim.keymap.set({ "i" }, "<C-s>e", function() ls.expand() end, { silent = true })
 
-        require("luasnip").config.setup {
-            update_events = 'TextChanged,TextChangedI',
-            enable_autosnippets = true,
-        }
-    end,
+            vim.keymap.set({ "i", "s" }, "<C-s>;", function() ls.jump(1) end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<C-s>,", function() ls.jump(-1) end, { silent = true })
+
+            vim.keymap.set({ "i", "s" }, "<C-E>", function()
+                if ls.choice_active() then
+                    ls.change_choice(1)
+                end
+            end, { silent = true })
+
+            require("luasnip.loaders.from_snipmate").load({ path = { "./my-snippets" } })
+
+            require("luasnip").config.setup {
+                update_events = 'TextChanged,TextChangedI',
+                enable_autosnippets = true,
+            }
+        end,
+    }
 }
