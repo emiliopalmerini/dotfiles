b93c0110cf17f7d08a61ea16c6256f309fa61914
emiliopalmerini <emilio.palmerini@proton.me>
2024-08-30 05:17:21 +0200

    feat(nvim): change :so

--- .config/nvim/lua/emilio/map.lua
+++ .config/nvim/lua/emilio/map.lua
@@ -7,7 +7,7 @@ local set = vim.keymap.set
 set("v", "J", ":m '>+1<CR>gv=gv")
 set("v", "K", ":m '<-2<CR>gv=gv")
 
-set("n", "J", "mzJ`z")
+ set("n", "J", "mzJ`z")
 set("n", "<C-d>", "<C-d>zz")
 set("n", "<C-u>", "<C-u>zz")
 set("n", "n", "nzzzv")
