6642d8f06680a84dee6ac9cfb3b68190cd5af956
emiliopalmerini <emilio.palmerini@proton.me>
2024-09-02 16:02:11 +0200

    update

--- .config/nvim/lua/custom/plugins/fugitive.lua
+++ .config/nvim/lua/custom/plugins/fugitive.lua
@@ -1,38 +1,36 @@
 return {
-    "tpope/vim-fugitive",
-    config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+	"tpope/vim-fugitive",
+	config = function()
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-        local emilio_fugitive = vim.api.nvim_create_augroup("emilio_fugitive", {})
+		local emilio_fugitive = vim.api.nvim_create_augroup("emilio_fugitive", {})
 
-        local autocmd = vim.api.nvim_create_autocmd
-        autocmd("BufWinEnter", {
-            group = emilio_fugitive,
-            pattern = "*",
-            callback = function()
-                if vim.bo.ft ~= "fugitive" then
-                    return
-                end
+		local autocmd = vim.api.nvim_create_autocmd
+		autocmd("BufWinEnter", {
+			group = emilio_fugitive,
+			pattern = "*",
+			callback = function()
+				if vim.bo.ft ~= "fugitive" then
+					return
+				end
 
-                local bufnr = vim.api.nvim_get_current_buf()
-                local opts = {buffer = bufnr, remap = false}
-                vim.keymap.set("n", "<leader>gp", function()
-                    vim.cmd.Git('push')
-                end, opts)
+				local bufnr = vim.api.nvim_get_current_buf()
+				local opts = { buffer = bufnr, remap = false }
+				vim.keymap.set("n", "<leader>gp", function()
+					vim.cmd.Git("push")
+				end, opts)
 
-                vim.keymap.set("n", "<leader>gf", function()
-                    vim.cmd.Git('push --force')
-                end, opts)
+				vim.keymap.set("n", "<leader>gf", function()
+					vim.cmd.Git("push --force")
+				end, opts)
 
-                -- rebase always
-                vim.keymap.set("n", "<leader>gP", function()
-                    vim.cmd.Git({'pull'})
-                end, opts)
+				-- rebase always
+				vim.keymap.set("n", "<leader>gP", function()
+					vim.cmd.Git({ "pull" })
+				end, opts)
 
-                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>gt", ":Git push -u origin ", opts);
-            end,
-        })
-end
+				vim.keymap.set("n", "<leader>gt", ":Git push -u origin ", opts)
+			end,
+		})
+	end,
 }
