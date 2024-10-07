4fe9736c1fced9950e941643acd210d07f39ee2d
emiliopalmerini <emilio.palmerini@proton.me>
2024-10-07 23:54:14 +0200

    feat(nvim): nix it

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -20,20 +20,10 @@ return {
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end, { desc = "Open [H]arpoon list" })
 
-		vim.keymap.set("n", "<leader>1", function()
-			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<leader>2", function()
-			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<leader>3", function()
-			harpoon:list():select(3)
-		end)
-		vim.keymap.set("n", "<leader>4", function()
-			harpoon:list():select(4)
-		end)
-		vim.keymap.set("n", "<leader>5", function()
-			harpoon:list():select(5)
-		end)
+		for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
+			vim.keymap.set("n", string.format("<space>%d", idx), function()
+				harpoon:list():select(idx)
+			end)
+		end
 	end,
 }
