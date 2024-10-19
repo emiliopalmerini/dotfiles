b198677500c27eb343e76a5a7f105a2bedfe1e92
emiliopalmerini <emilio.palmerini@proton.me>
2024-10-19 17:23:58 +0200

    update

--- .config/nvim/lua/custom/plugins/dap.lua
+++ .config/nvim/lua/custom/plugins/dap.lua
@@ -16,6 +16,7 @@ return {
 			require("dap-go").setup()
 
 			require("nvim-dap-virtual-text").setup({
+				-- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
 				display_callback = function(variable)
 					local name = string.lower(variable.name)
 					local value = string.lower(variable.value)
@@ -31,22 +32,35 @@ return {
 				end,
 			})
 
-			dap.adapters.coreclr = {
-				type = "executable",
-				command = "/usr/local/bin/netcoredbg/netcoredbg",
-				args = { "--interpreter=vscode" },
-			}
+			-- Handled by nvim-dap-go
+			-- dap.adapters.go = {
+			--   type = "server",
+			--   port = "${port}",
+			--   executable = {
+			--     command = "dlv",
+			--     args = { "dap", "-l", "127.0.0.1:${port}" },
+			--   },
+			-- }
 
-			dap.configurations.cs = {
-				{
-					type = "coreclr",
-					name = "launch - netcoredbg",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
-					end,
-				},
-			}
+			local elixir_ls_debugger = vim.fn.exepath("elixir-ls-debugger")
+			if elixir_ls_debugger ~= "" then
+				dap.adapters.mix_task = {
+					type = "executable",
+					command = elixir_ls_debugger,
+				}
+
+				dap.configurations.elixir = {
+					{
+						type = "mix_task",
+						name = "phoenix server",
+						task = "phx.server",
+						request = "launch",
+						projectDir = "${workspaceFolder}",
+						exitAfterTaskReturns = false,
+						debugAutoInterpretAllModules = false,
+					},
+				}
+			end
 
 			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
 			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
@@ -61,7 +75,7 @@ return {
 			vim.keymap.set("n", "<F3>", dap.step_over)
 			vim.keymap.set("n", "<F4>", dap.step_out)
 			vim.keymap.set("n", "<F5>", dap.step_back)
-			vim.keymap.set("n", "<F6>", dap.restart)
+			vim.keymap.set("n", "<F13>", dap.restart)
 
 			dap.listeners.before.attach.dapui_config = function()
 				ui.open()
