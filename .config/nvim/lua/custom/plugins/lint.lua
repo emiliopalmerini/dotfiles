5cfd77bdb9a02a36380b3e3118e3655193994693
emiliopalmerini <emilio.palmerini@proton.me>
2024-11-19 10:14:56 +0100

    feat: remove thefuck

--- .config/nvim/lua/custom/plugins/lint.lua
+++ .config/nvim/lua/custom/plugins/lint.lua
@@ -1,14 +1,10 @@
 return {
 	"mfussenegger/nvim-lint",
 	config = function()
+		-- Configura i linters per i file JavaScript e TypeScript
 		require("lint").linters_by_ft = {
 			javascript = { "eslint" },
 			typescript = { "eslint" },
 		}
-		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-			callback = function()
-				require("lint").try_lint()
-			end,
-		})
 	end,
 }
