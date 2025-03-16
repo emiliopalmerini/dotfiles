19be8d10ee79c5894d680f8bcdbabc38a24b5d50
emiliopalmerini <emiliopalmerini@proton.me>
2025-03-16 18:12:57 +0100

    feat(idun): remove logitech options+ -> manual installation

--- nix/hosts/idun/configuration.nix
+++ nix/hosts/idun/configuration.nix
@@ -11,21 +11,21 @@
 
   programs.zsh.enable = true;
   nixpkgs.config.allowUnfree = true;
-    environment.variables = {
-      EDITOR = "nvim";
-      TERM = "xterm-256color";
-    };
-    system.defaults = {
-      dock.autohide  = true;
-      dock.persistent-apps = [
-      ];
-      finder.FXPreferredViewStyle = "clmv";
-      loginwindow.GuestEnabled  = false;
-      NSGlobalDomain.AppleICUForce24HourTime = true;
-      NSGlobalDomain.AppleInterfaceStyle = "Dark";
-      NSGlobalDomain.KeyRepeat = 10;
-      NSGlobalDomain."com.apple.swipescrolldirection" = false;
-    };
+  environment.variables = {
+    EDITOR = "nvim";
+    TERM = "xterm-256color";
+  };
+  system.defaults = {
+    dock.autohide = true;
+    dock.persistent-apps = [
+    ];
+    finder.FXPreferredViewStyle = "clmv";
+    loginwindow.GuestEnabled = false;
+    NSGlobalDomain.AppleICUForce24HourTime = true;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+    NSGlobalDomain.KeyRepeat = 10;
+    NSGlobalDomain."com.apple.swipescrolldirection" = false;
+  };
 
 
   users.users.emiliopalmerini = {
@@ -33,55 +33,55 @@
     shell = pkgs.zsh;
   };
 
-    homebrew = {
-      enable = true;
-      brews = [
-        "mas"
-        "docker-compose"
-        "clamav"
-      ];
-      casks = [
-        "zen-browser"
-        "epic-games"
-        "obs"
-        "logi-options+"
-        "docker"
-        "vlc"
-       "syncthing"
-        "warp"
+  homebrew = {
+    enable = true;
+    brews = [
+      "mas"
+      "docker-compose"
+      "clamav"
+    ];
+    casks = [
+      "zen-browser"
+      "epic-games"
+      "obs"
+      "docker"
+      "vlc"
+      "syncthing"
+      "warp"
       "obsidian"
-      ];
-      masApps = {
+    ];
+    masApps = {
       "CapCut" = 1500855883;
       "HomeAssistant" = 1099568401;
-      };
-      onActivation.cleanup = "zap";
     };
+    onActivation.cleanup = "zap";
+  };
 
-    system.activationScripts.applications.text = let
+  system.activationScripts.applications.text =
+    let
       env = pkgs.buildEnv {
         name = "system-applications";
         paths = config.environment.systemPackages;
         pathsToLink = "/Applications";
       };
     in
-      pkgs.lib.mkForce ''
-  # Set up applications.
-  echo "setting up /Applications..." >&2
-  rm -rf /Applications/Nix\ Apps
-  mkdir -p /Applications/Nix\ Apps
-  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-  while read -r src; do
-    app_name=$(basename "$src")
-    echo "copying $src" >&2
-        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-  done
-      '';
+    pkgs.lib.mkForce ''
+      # Set up applications.
+      echo "setting up /Applications..." >&2
+      rm -rf /Applications/Nix\ Apps
+      mkdir -p /Applications/Nix\ Apps
+      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read -r src; do
+        app_name=$(basename "$src")
+        echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+    '';
 
   home-manager = {
     # useGlobalPkgs = true;
     # useUserPackages = true;
-    extraSpecialArgs = {inherit inputs;};
+    extraSpecialArgs = { inherit inputs; };
     backupFileExtension = "backup";
     users = {
       "emiliopalmerini" = import ./home.nix;
