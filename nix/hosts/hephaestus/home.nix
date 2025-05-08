7df25bb876a064db2a461cb8d0ac8090950e5cdd
emiliopalmerini <emilio.palmerini@codiceplastico.com>
2025-05-08 15:59:34 +0200

    refactor(hepheastus): reorder enable

--- nix/hosts/hephaestus/home.nix
+++ nix/hosts/hephaestus/home.nix
@@ -14,42 +14,36 @@
     stateVersion = "24.11";
   };
 
-  firefox.enable = false;
+  bruno.enable = true;
   chrome.enable = true;
-
-  dbeaver.enable = true;
+  dbeaver.enable = false;
+  dotnet.enable = true;
+  firefox.enable = false;
+  gcc.enable = false;
+  ghostty.enable = true;
   git = {
     enable = true;
     userEmail = "emilio.palmerini@codiceplastico.com";
     userName = "emiliopalmerini";
   };
+  go.enable = true;
+  kitty.enable = true;
+  lazygit.enable = true;
+  make.enable = true;
+  mongodb.enable = true;
+  neovim.enable = false;
+  nvf.enable = true;
+  obsidian.enable = true;
+  office.enable = true;
+  postman.enable = true;
+  programs.home-manager.enable = true;
+  shell.enable = true;
+  slack.enable = true;
+  tmux.enable = true;
 
   home.packages = with pkgs; [
     nodejs_23
   ];
 
-  kitty.enable = true;
-  ghostty.enable = true;
-
-  go.enable = true;
-  gcc.enable = false;
-  dotnet.enable = true;
-
-  lazygit.enable = true;
-  neovim.enable = false;
-  nvf.enable = true;
-  obsidian.enable = true;
-  office.enable = true;
-  shell.enable = true;
-  tmux.enable = true;
-  make.enable = true;
-
-  mongodb.enable = true;
-  slack.enable = true;
-  bruno.enable = true;
-  postman.enable = true;
-
   nixpkgs.config.allowUnfree = true;
-
-  programs.home-manager.enable = true;
 }
