e016e022e717a9803bf0eed3d40f413541ecbc96
emiliopalmerini <emilio.palmerini@codiceplastico.com>
2025-05-13 12:03:12 +0200

    feat(neovim): enlarge telescope preview

--- nix/modules/home/neovim/plugin/telescope.lua
+++ nix/modules/home/neovim/plugin/telescope.lua
@@ -1,17 +1,45 @@
 require("telescope").setup({
 	defaults = {
-		pickers = {
-			find_files = {
-				find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		-- usa layout orizzontale
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = {
+				preview_width = 0.5,
+				prompt_position = "top",
+				width = 0.9,
+				height = 0.9,
+			},
+			vertical = {
+				mirror = false,
 			},
 		},
-		extensions = {
-			fzf = {
-				fuzzy = true, -- false will only do exact matching
-				override_generic_sorter = true, -- override the generic sorter
-				override_file_sorter = true, -- override the file sorter
-				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		preview_cutoff = 40,
+	},
+
+	pickers = {
+		find_files = {
+			-- comando di ricerca
+			find_command = {
+				"rg",
+				"--files",
+				"--hidden",
+				"--glob",
+				"!**/.git/*",
 			},
+			layout_config = {
+				horizontal = {
+					preview_width = 0.55,
+				},
+			},
+		},
+	},
+
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
 		},
 	},
 })
