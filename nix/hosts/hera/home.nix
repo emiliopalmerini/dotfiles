e7eeccbdfa4453006ff1b6eaa93042934944a73f
emiliopalmerini <emilio.palmerini@gmail>
2025-05-18 11:27:45 +0200

    feat(hera): setup

---
+++ nix/hosts/hera/home.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./../../modules/home
+  ];
+
+  home.username = "prometeo";
+  home.homeDirectory = "/home/prometeo";
+
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  firefox.enable = true;
+
+  git.enable = true;
+  git.userEmail = "emilio.palmerini@gmail";
+  git.userName = "emiliopalmerini";
+
+  lazygit.enable = true;
+  neovim.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.home-manager.enable = true;
+}
