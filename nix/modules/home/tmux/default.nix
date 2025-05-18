749fd653ff819c094cc5abb08aa1506940233e68
emiliopalmerini <emiliopalmerini@proton.me>
2025-05-18 11:38:20 +0200

    fix(tmux): fix tmux on idun

--- nix/modules/home/tmux/default.nix
+++ nix/modules/home/tmux/default.nix
@@ -20,6 +20,7 @@ in {
         set -g default-terminal "xterm-256color"
         set -ga terminal-overrides ",*256col*:Tc"
         set-environment -g COLORTERM "truecolor"
+        set-option -g default-shell ${pkgs.zsh}/bin/zsh
 
         set -g mouse on
 
@@ -28,27 +29,6 @@ in {
         set -g pane-base-index 1
         set-window-option -g pane-base-index 1
         set-option -g renumber-windows on
-
-        # Use Alt-arrow keys without prefix key to switch panes
-        bind  h select-pane -L
-        bind  l select-pane -R
-        bind  k select-pane -U
-        bind  j select-pane -D
-
-        # Use Alt-arrow keys without prefix key to switch panes
-        bind -n M-Left select-pane -L
-        bind -n M-Right select-pane -R
-        bind -n M-Up select-pane -U
-        bind -n M-Down select-pane -D
-
-        # Shift arrow to switch windows
-        bind -n S-Left previous-window
-        bind -n S-Right next-window
-
-        # Shift Alt vim keys to switch windows
-        bind -n M-H previous-window
-        bind -n M-L next-window
-
 
         # set vi-mode
         set-window-option -g mode-keys vi
