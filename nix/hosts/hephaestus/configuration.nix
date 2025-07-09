002cb6c0728a3558ff69ad8f48afbe0c8536a95f
emiliopalmerini <emiliopalmerini@proton.me>
2025-07-09 23:25:34 +0200

    feat(hepheastus): add vpn

--- nix/hosts/hephaestus/configuration.nix
+++ nix/hosts/hephaestus/configuration.nix
@@ -1,153 +1,171 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  pkgs,
-  inputs,
-  ...
-}: let
+{ pkgs
+, inputs
+, ...
+}:
+let
   userName = "emilio";
-in {
-    imports = [
-      ./hardware-configuration.nix
-      ../../modules/nixos
-      inputs.home-manager.nixosModules.default
-    ];
-    boot = {
-      # Bootloader.
-      kernelParams = ["kvm.enable_virt_at_load=0"];
-      loader.systemd-boot.enable = true;
-      loader.efi.canTouchEfiVariables = true;
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/nixos
+    inputs.home-manager.nixosModules.default
+  ];
+  boot = {
+    # Bootloader.
+    kernelParams = [ "kvm.enable_virt_at_load=0" ];
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
 
-      initrd.luks.devices."luks-68c92b43-cf28-46bf-9a0a-96a2290dcdac".device = "/dev/disk/by-uuid/68c92b43-cf28-46bf-9a0a-96a2290dcdac";
-    };
-    networking.hostName = "hephaestus"; # Define your hostname.
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    initrd.luks.devices."luks-68c92b43-cf28-46bf-9a0a-96a2290dcdac".device = "/dev/disk/by-uuid/68c92b43-cf28-46bf-9a0a-96a2290dcdac";
+  };
+  networking.hostName = "hephaestus"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
 
-    # Configure network proxy if necessary
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    # Enable networking
-    networking.networkmanager.enable = true;
-    system = {
-      autoUpgrade.enable = true;
-      autoUpgrade.dates = "weekly";
+  # Enable networking
+  networking.networkmanager.enable = true;
+  system = {
+    autoUpgrade.enable = true;
+    autoUpgrade.dates = "weekly";
 
-      # This value determines the NixOS release from which the default
-      # settings for stateful data, like file locations and database versions
-      # on your system were taken. It‘s perfectly fine and recommended to leave
-      # this value at the release version of the first install of this system.
-      # Before changing this value read the documentation for this option
-      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-      stateVersion = "24.11";
-    };
-    nix = {
-      gc = {
-        automatic = true;
-        dates = "daily";
-        options = "--delete-older-than 10d";
-      };
-
-      settings.auto-optimise-store = true;
-      settings.experimental-features = ["nix-command" "flakes"];
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    stateVersion = "24.11";
+  };
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 10d";
     };
 
-    virtualisation.virtualbox.host.enable = true;
-    virtualisation.virtualbox.host.enableExtensionPack = true;
+    settings.auto-optimise-store = true;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
 
-    time.timeZone = "Europe/Rome";
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
 
-    i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "Europe/Rome";
 
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "it_IT.UTF-8";
-      LC_IDENTIFICATION = "it_IT.UTF-8";
-      LC_MEASUREMENT = "it_IT.UTF-8";
-      LC_MONETARY = "it_IT.UTF-8";
-      LC_NAME = "it_IT.UTF-8";
-      LC_NUMERIC = "it_IT.UTF-8";
-      LC_PAPER = "it_IT.UTF-8";
-      LC_TELEPHONE = "it_IT.UTF-8";
-      LC_TIME = "it_IT.UTF-8";
-    };
-    services = {
-      xserver = {
-        enable = true;
-        displayManager.gdm.enable = true;
-        desktopManager.gnome.enable = true;
-        xkb = {
-          layout = "us";
-          variant = "intl";
-        };
-      };
+  i18n.defaultLocale = "en_US.UTF-8";
 
-      # Enable CUPS to print documents.
-      printing.enable = true;
-      pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        # If you want to use JACK applications, uncomment this
-        #jack.enable = true;
-
-        # use the example session manager (no others are packaged yet so this is enabled by default,
-        # no need to redefine it in your config for now)
-        #media-session.enable = true;
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "it_IT.UTF-8";
+    LC_IDENTIFICATION = "it_IT.UTF-8";
+    LC_MEASUREMENT = "it_IT.UTF-8";
+    LC_MONETARY = "it_IT.UTF-8";
+    LC_NAME = "it_IT.UTF-8";
+    LC_NUMERIC = "it_IT.UTF-8";
+    LC_PAPER = "it_IT.UTF-8";
+    LC_TELEPHONE = "it_IT.UTF-8";
+    LC_TIME = "it_IT.UTF-8";
+  };
+  services = {
+    # X server and keyboard
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "intl";
       };
     };
 
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
+    # Moved out of services.xserver.* per renames in 24.11
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
 
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.${userName} = {
-      isNormalUser = true;
-      description = "${userName}";
-      extraGroups = ["networkmanager" "wheel" "docker" "vboxusers"];
-      shell = pkgs.zsh;
-      ignoreShellProgramCheck = true;
+    # Enable CUPS to print documents.
+    printing.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
     };
+  };
 
-    # Install firefox.
-    programs.firefox.enable = true;
+  environment.gnome.excludePackages = (with pkgs; [
+    atomix # puzzle game
+    cheese # webcam tool
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gedit # text editor
+    gnome-characters
+    gnome-music
+    gnome-photos
+    gnome-terminal
+    gnome-tour
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+    totem # video player
+  ]);
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
 
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment.systemPackages = with pkgs; [
-      networkmanager
-      networkmanager-sstp
-      quickemu
-      vagrant
-      mono
-      msbuild
-      docker-compose
-      lazydocker
-    ];
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${userName} = {
+    isNormalUser = true;
+    description = "${userName}";
+    extraGroups = [ "networkmanager" "wheel" "docker" "vboxusers" ];
+    shell = pkgs.zsh;
+    ignoreShellProgramCheck = true;
+  };
 
-    nixpkgs.config.permittedInsecurePackages = [
-      "dotnet-sdk-6.0.428"
-      "dotnet-runtime-6.0.36"
-    ];
+  # Install firefox.
+  programs.firefox.enable = true;
 
-    tailscale.enable = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-    home-manager = {
-      extraSpecialArgs = {inherit inputs;};
-      users = {
-        "${userName}" = import ./home.nix;
-      };
-      backupFileExtension = "bak";
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    networkmanager
+    sstp
+    ppp
+    quickemu
+    vagrant
+    mono
+    msbuild
+    docker-compose
+    lazydocker
+  ];
+
+  tailscale.enable = true;
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "${userName}" = import ./home.nix;
     };
+    backupFileExtension = "bak";
+  };
 
   virtualisation.docker = {
     enable = true;
@@ -177,6 +195,16 @@ in {
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
   programs.zsh.enable = true;
+  services.flatpak.enable = true;
   users.defaultUserShell = pkgs.zsh; # Did you read the comment?
+
+  # Enable generic Docker module and it-tools container
+  docker.enable = true;
 }
