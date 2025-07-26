941a7e86dcf8e35ced8b8981792ad8db3b2ea2f6
emiliopalmerini <emiliopalmerini@gmail.com>
2025-07-26 09:47:00 +0200

    fix(eris): tmux don't use sh on eris

--- nix/modules/home/tmux/default.nix
+++ nix/modules/home/tmux/default.nix
@@ -17,10 +17,8 @@ in {
       sensibleOnTop = true;
       # TODO: binding per muoversi senza soluzione di continuità con nvim
       extraConfig = ''
-set-option -g default-shell ${pkgs.zsh}/bin/zsh
 set-option -sa terminal-overrides ",xterm*:Tc"
 set -g mouse on
-
 
 # Vim style pane selection
 bind h select-pane -L
@@ -34,19 +32,11 @@ set -g pane-base-index 1
 set-window-option -g pane-base-index 1
 set-option -g renumber-windows on
 
-# Use Alt-arrow keys without prefix key to switch panes
-bind -n M-Left select-pane -L bind -n M-Right select-pane -R
-bind -n M-Up select-pane -U
-bind -n M-Down select-pane -D
-
-# Shift arrow to switch windows
-bind -n S-Left  previous-window
-bind -n S-Right next-window
-
 # Shift Alt vim keys to switch windows
 bind -n M-H previous-window
 bind -n M-L next-window
 
+set-option -g default-command "exec zsh"
 
 # set vi-mode
 set-window-option -g mode-keys vi
