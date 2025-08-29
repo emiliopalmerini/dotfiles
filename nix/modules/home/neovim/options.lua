e6149b37f87c85c3a2cb05787a9e498a73dbc544
emiliopalmerini <emilio.palmerini@codiceplastico.com>
2025-08-29 11:59:27 +0200

    chore(neovim): improve which-key mappings

--- nix/modules/home/neovim/options.lua
+++ nix/modules/home/neovim/options.lua
@@ -41,40 +41,40 @@ vim.g.have_nerd_font = true
 vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join line below, keep cursor" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half-page down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half-page up and center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result centered" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Prev search result centered" })
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste over without yanking" })
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboard" })
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete to black hole register" })
 
-vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "Q", "<nop>", { desc = "Disable Ex mode" })
 
-vim.keymap.set("n", "[N", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "]J", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "[N", "<cmd>cnext<CR>zz", { desc = "Next quickfix item (center)" })
+vim.keymap.set("n", "]J", "<cmd>cprev<CR>zz", { desc = "Prev quickfix item (center)" })
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next location list item (center)" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Prev location list item (center)" })
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>tf", ":e temp.txt<Cr>")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search/replace word under cursor" })
+vim.keymap.set("n", "<leader>tf", ":e temp.txt<Cr>", { desc = "Open temp.txt" })
 
-vim.keymap.set("n", "<left>", "<c-w>5<")
-vim.keymap.set("n", "<right>", "<c-w>5>")
-vim.keymap.set("n", "<up>", "<C-W>+")
-vim.keymap.set("n", "<down>", "<C-W>-")
+vim.keymap.set("n", "<left>", "<c-w>5<", { desc = "Resize window 5 cols left" })
+vim.keymap.set("n", "<right>", "<c-w>5>", { desc = "Resize window 5 cols right" })
+vim.keymap.set("n", "<up>", "<C-W>+", { desc = "Increase window height" })
+vim.keymap.set("n", "<down>", "<C-W>-", { desc = "Decrease window height" })
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
 local builtin = require("telescope.builtin")
 
@@ -106,7 +106,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 })
 
 -- Easily hit escape in terminal mode.
-vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Exit terminal mode" })
 
 -- Open a terminal at the bottom of the screen with a fixed height.
 vim.keymap.set("n", ",st", function()
@@ -115,12 +115,12 @@ vim.keymap.set("n", ",st", function()
 	vim.api.nvim_win_set_height(0, 12)
 	vim.wo.winfixheight = true
 	vim.cmd.term()
-end)
+end, { desc = "Split bottom terminal (12 lines)" })
 
 -- Plugins keymap
 
 --undotree
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
 
 -- harpoon (guarded)
 do
@@ -129,34 +129,34 @@ do
 		harpoon:setup()
 		vim.keymap.set("n", "<leader>h", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
+		end, { desc = "Harpoon: Toggle quick menu" })
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
-		end)
+		end, { desc = "Harpoon: Add file" })
 		vim.keymap.set("n", "<leader>1", function()
 			harpoon:list():select(1)
-		end)
+		end, { desc = "Harpoon: Go to file 1" })
 		vim.keymap.set("n", "<leader>2", function()
 			harpoon:list():select(2)
-		end)
+		end, { desc = "Harpoon: Go to file 2" })
 		vim.keymap.set("n", "<leader>3", function()
 			harpoon:list():select(3)
-		end)
+		end, { desc = "Harpoon: Go to file 3" })
 		vim.keymap.set("n", "<leader>4", function()
 			harpoon:list():select(4)
-		end)
+		end, { desc = "Harpoon: Go to file 4" })
 		-- Toggle previous & next buffers stored within Harpoon list
 		vim.keymap.set("n", "<C-S-P>", function()
 			harpoon:list():prev()
-		end)
+		end, { desc = "Harpoon: Previous mark" })
 		vim.keymap.set("n", "<C-S-N>", function()
 			harpoon:list():next()
-		end)
+		end, { desc = "Harpoon: Next mark" })
 	end
 end
 
 --fugitive
-vim.keymap.set("n", "<leader>gf", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gf", vim.cmd.Git, { desc = "Git: Open Fugitive" })
 local emilio_fugitive = vim.api.nvim_create_augroup("emilio_fugitive", {})
 
 local autocmd = vim.api.nvim_create_autocmd
@@ -172,18 +172,18 @@ autocmd("BufWinEnter", {
 		local opts = { buffer = bufnr, remap = false }
 		vim.keymap.set("n", "<leader>gp", function()
 			vim.cmd.Git("push")
-		end, opts)
+		end, vim.tbl_extend("force", opts, { desc = "Git: Push" }))
 
 		vim.keymap.set("n", "<leader>gf", function()
 			vim.cmd.Git("push --force")
-		end, opts)
+		end, vim.tbl_extend("force", opts, { desc = "Git: Force push" }))
 
 		-- rebase always
 		vim.keymap.set("n", "<leader>gP", function()
 			vim.cmd.Git({ "pull" })
-		end, opts)
+		end, vim.tbl_extend("force", opts, { desc = "Git: Pull (rebase)" }))
 
-		vim.keymap.set("n", "<leader>gt", ":Git push -u origin ", opts)
+		vim.keymap.set("n", "<leader>gt", ":Git push -u origin ", vim.tbl_extend("force", opts, { desc = "Git: Push set upstream" }))
 	end,
 })
 
@@ -219,7 +219,7 @@ vim.keymap.set("n", "<leader>zz", function()
 	vim.wo.wrap = true
 	vim.wo.number = true
 	vim.wo.rnu = true
-end)
+end, { desc = "Zen Mode (width 100)" })
 
 vim.keymap.set("n", "<leader>zZ", function()
 	require("zen-mode").setup({
@@ -233,12 +233,12 @@ vim.keymap.set("n", "<leader>zZ", function()
 	vim.wo.number = false
 	vim.wo.rnu = false
 	vim.opt.colorcolumn = "0"
-end)
+end, { desc = "Zen Mode (width 80)" })
 
 --refactoring
 vim.keymap.set({ "n", "x" }, "<leader>rr", function()
 	require("telescope").extensions.refactoring.refactors()
-end)
+end, { desc = "Refactor: Select action" })
 
 -- vim-tmux-navigator
 local opts = { noremap = true, silent = true }
@@ -246,8 +246,8 @@ local opts = { noremap = true, silent = true }
 vim.g.tmux_navigator_no_mappings = 1
 
 -- mappe in Lua verso i comandi VimL che il plugin fornisce
-vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", opts)
-vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", opts)
-vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", opts)
-vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", opts)
-vim.keymap.set("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", opts)
+vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { noremap = true, silent = true, desc = "Tmux: Move left" })
+vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { noremap = true, silent = true, desc = "Tmux: Move down" })
+vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { noremap = true, silent = true, desc = "Tmux: Move up" })
+vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { noremap = true, silent = true, desc = "Tmux: Move right" })
+vim.keymap.set("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", { noremap = true, silent = true, desc = "Tmux: Previous pane" })
