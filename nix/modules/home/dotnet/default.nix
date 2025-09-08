f03e46679cc1c5907dd893db256a21aa94bb6887
emiliopalmerini <emilio.palmerini@codiceplastico.com>
2025-09-08 09:46:00 +0200

    fix(dotnet): support multiple sdks at once in home-manager

--- nix/modules/home/dotnet/default.nix
+++ nix/modules/home/dotnet/default.nix
@@ -1,18 +1,41 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
+{ lib
+, config
+, pkgs
+, ...
 }: {
   options.dotnet.enable = lib.mkEnableOption "Enable .NET SDKS";
 
   config = lib.mkIf config.dotnet.enable {
-    home.packages = with pkgs; [
-      dotnet-sdk_6
-      dotnet-sdk_8
-      dotnet-sdk_9
-      dotnetPackages.Nuget
-      netcoredbg
+    home.packages =
+      let
+        dotnet-combined = (with pkgs.dotnetCorePackages; combinePackages [
+          sdk_6_0
+          sdk_8_0
+          sdk_9_0
+        ]).overrideAttrs (finalAttrs: previousAttrs: {
+          postBuild = (previousAttrs.postBuild or "") + ''
+            for i in $out/sdk/*
+            do
+              i=$(basename $i)
+              mkdir -p $out/metadata/workloads/''${i/-*}
+              touch $out/metadata/workloads/''${i/-*}/userlocal
+            done
+          '';
+        });
+      in
+      [
+        dotnet-combined
+        pkgs.dotnetPackages.Nuget
+        pkgs.netcoredbg
+      ];
+
+    home.sessionVariables = {
+      DOTNET_ROOT = "${pkgs.dotnetCorePackages.combinePackages [ pkgs.dotnetCorePackages.sdk_6_0 pkgs.dotnetCorePackages.sdk_8_0 pkgs.dotnetCorePackages.sdk_9_0 ]}";
+    };
+
+    nixpkgs.config.permittedInsecurePackages = [
+      "dotnet-sdk-6.0.428"
+      "dotnet-runtime-6.0.36"
     ];
   };
 }
