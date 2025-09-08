a3380996d3468e492ac152611c4d6d6949c0bae0
emiliopalmerini <emilio.palmerini@codiceplastico.com>
2025-09-08 09:47:31 +0200

    refactor(dotnet): move sdks list and combined to variables

--- nix/modules/home/dotnet/default.nix
+++ nix/modules/home/dotnet/default.nix
@@ -2,40 +2,44 @@
 , config
 , pkgs
 , ...
-}: {
+}:
+let
+  dotnetSdks = with pkgs.dotnetCorePackages; [
+    sdk_6_0
+    sdk_8_0
+    sdk_9_0
+  ];
+
+  insecurePackages = [
+    "dotnet-sdk-6.0.428"
+    "dotnet-runtime-6.0.36"
+  ];
+
+  dotnet-combined = (pkgs.dotnetCorePackages.combinePackages dotnetSdks).overrideAttrs (finalAttrs: previousAttrs: {
+    postBuild = (previousAttrs.postBuild or "") + ''
+      for i in $out/sdk/*
+      do
+        i=$(basename $i)
+        mkdir -p $out/metadata/workloads/''${i/-*}
+        touch $out/metadata/workloads/''${i/-*}/userlocal
+      done
+    '';
+  });
+in
+{
   options.dotnet.enable = lib.mkEnableOption "Enable .NET SDKS";
 
   config = lib.mkIf config.dotnet.enable {
-    home.packages =
-      let
-        dotnet-combined = (with pkgs.dotnetCorePackages; combinePackages [
-          sdk_6_0
-          sdk_8_0
-          sdk_9_0
-        ]).overrideAttrs (finalAttrs: previousAttrs: {
-          postBuild = (previousAttrs.postBuild or "") + ''
-            for i in $out/sdk/*
-            do
-              i=$(basename $i)
-              mkdir -p $out/metadata/workloads/''${i/-*}
-              touch $out/metadata/workloads/''${i/-*}/userlocal
-            done
-          '';
-        });
-      in
-      [
-        dotnet-combined
-        pkgs.dotnetPackages.Nuget
-        pkgs.netcoredbg
-      ];
+    home.packages = [
+      dotnet-combined
+      pkgs.dotnetPackages.Nuget
+      pkgs.netcoredbg
+    ];
 
     home.sessionVariables = {
-      DOTNET_ROOT = "${pkgs.dotnetCorePackages.combinePackages [ pkgs.dotnetCorePackages.sdk_6_0 pkgs.dotnetCorePackages.sdk_8_0 pkgs.dotnetCorePackages.sdk_9_0 ]}";
+      DOTNET_ROOT = "${pkgs.dotnetCorePackages.combinePackages dotnetSdks}";
     };
 
-    nixpkgs.config.permittedInsecurePackages = [
-      "dotnet-sdk-6.0.428"
-      "dotnet-runtime-6.0.36"
-    ];
+    nixpkgs.config.permittedInsecurePackages = insecurePackages;
   };
 }
