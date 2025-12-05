47aa661927969405fcfcc74c8309a531cf60842f
emiliopalmerini <emilio.palmerini@gmail.com>
2025-12-05 16:42:07 +0100

    feat(home): expose port in abitudini module

--- nix/modules/home/abitudini/default.nix
+++ nix/modules/home/abitudini/default.nix
@@ -9,11 +9,34 @@ with lib; let
 in
 {
   options = {
-    abitudini.enable = mkEnableOption "Enable abitudini";
+    abitudini = {
+      enable = mkEnableOption "Enable abitudini";
+      port = mkOption {
+        type = types.port;
+        default = 8380;
+        description = "Port to listen on";
+      };
+    };
   };
+  
   config = mkIf cfg.enable {
     home.packages = [
       inputs.abitudini.packages.${pkgs.stdenv.system}.abitudini
     ];
+
+    systemd.user.services.abitudini = {
+      Unit = {
+        Description = "Abitudini habit tracker";
+        After = [ "network.target" ];
+      };
+      Service = {
+        ExecStart = "${inputs.abitudini.packages.${pkgs.stdenv.system}.abitudini}/bin/abitudini -p ${toString cfg.port}";
+        Restart = "on-failure";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
   };
 }
+
