ea65d69c9209f18783d60857ba19594a3a6a0a04
emiliopalmerini <emilio.palmerini@vivaticket.com>
2025-12-29 11:01:28 +0100

    fix(neovim): swap telescope keymaps C-n/C-p for selection, C-j/C-k for history

--- nix/modules/home/neovim/plugins/telescope.lua
+++ nix/modules/home/neovim/plugins/telescope.lua
@@ -44,10 +44,10 @@
           },
           mappings = {
             i = {
-              ["<C-n>"] = actions.cycle_history_next,
-              ["<C-p>"] = actions.cycle_history_prev,
-              ["<C-j>"] = actions.move_selection_next,
-              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-n>"] = actions.move_selection_next,
+              ["<C-p>"] = actions.move_selection_previous,
+              ["<C-j>"] = actions.cycle_history_next,
+              ["<C-k>"] = actions.cycle_history_prev,
             },
             n = {
               ["q"] = actions.close,
