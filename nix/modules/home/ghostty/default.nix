4d98d5e7efaa0cc232115fcbd0b421a2adf072c8
emiliopalmerini <emilio.palmerini@gmail.com>
2026-01-03 12:27:43 +0100

    feat(ghostty): add vim-like keybindings for splits and tabs

--- nix/modules/home/ghostty/default.nix
+++ nix/modules/home/ghostty/default.nix
@@ -14,6 +14,24 @@ in
       font-family = "Hack Nerd Font Mono"
       font-feature = -calt -liga -dlig
       theme = tokyonight-storm
+
+      # Window/tab management (tmux-like with Ctrl+b prefix)
+      keybind = ctrl+b>c=new_tab
+      keybind = ctrl+b>"=new_split:down
+      keybind = ctrl+b>%=new_split:right
+
+      # Switch tabs (Alt+Shift+H/L like tmux)
+      keybind = alt+shift+h=previous_tab
+      keybind = alt+shift+l=next_tab
+
+      # Navigate splits (vim-like Ctrl+hjkl)
+      keybind = ctrl+h=goto_split:left
+      keybind = ctrl+j=goto_split:bottom
+      keybind = ctrl+k=goto_split:top
+      keybind = ctrl+l=goto_split:right
+
+      # Close split/tab
+      keybind = ctrl+b>x=close_surface
     '';
   };
 }
